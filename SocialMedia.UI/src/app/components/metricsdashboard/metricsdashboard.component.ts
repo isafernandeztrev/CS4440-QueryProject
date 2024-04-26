4d9d30c03b0aa6fece657322f5f106abdbdb4ec6
Shen, Edwin <eshen36@gatech.edu>
2024-04-26 16:23:34 -0400

    updata

--- SocialMedia.UI/src/app/components/metricsdashboard/metricsdashboard.component.ts
+++ SocialMedia.UI/src/app/components/metricsdashboard/metricsdashboard.component.ts
@@ -1,21 +1,10 @@
-// import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-metricsdashboard',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './metricsdashboard.component.html',
-//   styleUrl: './metricsdashboard.component.css'
-// })
-// export class MetricsdashboardComponent {
-
-// }
-
-// src/app/components/metricsdashboard/metricsdashboard.component.ts
 import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Metric } from '../../models/metric'; // Update the import path as necessary
 import { CommonModule } from '@angular/common';
+import { Falsy } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-metricsdashboard',
@@ -56,13 +45,55 @@ import { CommonModule } from '@angular/common';
 export class MetricsdashboardComponent {
   selectedMetric: string = 'followerGrowth';
   tableData: any[] | null = null;
+  tableData1: boolean = false;
+  tableData2: boolean = false;
+  tableData3: boolean = false;
 
-  constructor() {}
+  // constructor(){};
+  constructor(private http: HttpClient) {
+    this.fetchFollowerData();
+  }
+  likesTikTokVideo: number = 100;
+  likesInstagramVideo: number = 200;
+  likesYouTubeVideo: number = 300;
+  commentsTikTokVideo: number = 50;
+  commentsInstagramVideo: number = 100;
+  commentsYouTubeVideo: number = 150;
+  likesTikTokPosts: number = 100;
+  likesInstagramPosts: number = 200;
+  likesYouTubePosts: number = 300;
+  commentsTikTokPosts: number = 50;
+  commentsInstagramPosts: number = 100;
+  commentsYouTubePosts: number = 150;
+  likesTikTok: number = 100;
+  likesInstagram: number = 200;
+  likesYouTube: number = 300;
+  commentsTikTok: number = 50;
+  commentsInstagram: number = 100;
+  commentsYouTube: number = 150;
+  followersTikTok: any[] = [100, 200, 300, 400, 500];
+  followersInstagram: any[] = [100, 200, 300, 400, 500];
+  followersYouTube: any[] = [100, 200, 300, 400, 500];
+  
+
+  
+  fetchFollowerData() {
+    this.http.get<any>('http://localhost:5064/Dashboard/getTotalNumOfFollowers').subscribe({
+      next: (data) => {
+        // Assuming data comes in the form of { likesTikTok: 100, likesInstagram: 200, ... }
+        this.followersTikTok = data[0];
+        this.followersInstagram = data[1];
+        this.followersYouTube = data[2];
+        // this.updateTableData();
+      },
+      error: (err) => console.error('Error fetching static data:', err)
+    });
+  }
 
   staticTableData: any[] = [
-    [{ value: 'Data 1-1' }, { value: 'Data 1-2' }, { value: 'Data 1-3' }],
-    [{ value: 'Data 2-1' }, { value: 'Data 2-2' }, { value: 'Data 2-3' }],
-    [{ value: 'Data 3-1' }, { value: 'Data 3-2' }, { value: 'Data 3-3' }],
+    [{ value: 'Tiktok' }, { value: 'Instagram' }, { value: 'Youtube' }],
+    [{ value: this.likesTikTok+"asda" }, { value: this.likesInstagram }, { value: this.likesYouTube }],
+    [{ value: this.commentsTikTok }, { value: this.commentsInstagram }, { value: this.commentsYouTube}],
   ];
 
 
@@ -70,21 +101,33 @@ export class MetricsdashboardComponent {
   fetchData() {
     switch (this.selectedMetric) {
       case 'followerGrowth':
+        this.tableData1 = true
+        this.tableData2 = false;
+        this.tableData3 = false;
         this.tableData = [
-          { category: 'January', value: '200' },
-          { category: 'February', value: '450' }
-        ];
+          { category: 'past 5 month', value: ['200', '300', '400']},
+          { category: 'past 4 month', value: '450' },
+          { category: 'past 3 month', value: '450' },
+          { category: 'past 2 month', value: '450' },
+          { category: 'past 1 month', value: '450' }
+        ]
         break;
       case 'likesPerContentType':
+        this.tableData1 = false;
+        this.tableData2 = true;
+        this.tableData3 = false;
         this.tableData = [
-          { category: 'Blogs', value: '150' },
-          { category: 'Videos', value: '300' }
+          { category: 'Posts', value: [1,2,3] },
+          { category: 'Videos', value:[1,2,3] }
         ];
         break;
       case 'commentsPerContentType':
+        this.tableData1 = false;
+        this.tableData2 = false;
+        this.tableData3 = true;
         this.tableData = [
-          { category: 'Blogs', value: '75' },
-          { category: 'Videos', value: '120' }
+          { category: 'Posts', value: [1,2,3] },
+          { category: 'Videos', value: [1,2,3] }
         ];
         break;
       default:
